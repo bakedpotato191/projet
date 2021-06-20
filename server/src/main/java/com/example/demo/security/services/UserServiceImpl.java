fd6193ee060e62234e72a2dd77efeedd18517a89
bakedpotato191 <vladislavp@pm.me>
2021-06-21 01:41:35 +0200

    remove unused dependencies

--- server/src/main/java/com/example/demo/security/services/UserServiceImpl.java
+++ server/src/main/java/com/example/demo/security/services/UserServiceImpl.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
