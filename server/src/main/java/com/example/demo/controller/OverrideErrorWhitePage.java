36cc618cf18d37b6cea9316092ee92684967dd10
bakedpotato191 <vladislavp@pm.me>
2021-07-13 16:09:56 +0200

    Replace whitelabel error page by an api error

---
+++ server/src/main/java/com/example/demo/controller/OverrideErrorWhitePage.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.apierror.ApiError;
+
+@RestController
+public class OverrideErrorWhitePage implements ErrorController {
+	
+	@RequestMapping(value = "/error" , produces = MediaType.APPLICATION_JSON_VALUE)
+	public ApiError handleError() {
+		return new ApiError(HttpStatus.NOT_FOUND, "Requested resource was not found on this server");
+	}
+}
