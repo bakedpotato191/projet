fe4496fb788c753a1d460f9d01f1f8f6bda80051
bakedpotato191 <vladislavp@Pm.me>
2021-08-26 16:57:58 +0200

    use profile button instead

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +12,15 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
 export class HeaderComponent implements OnInit {
 
   isLoggedIn = false;
-  username?: string;
+  username!: string;
+  avatar!: string;
 
   position = new FormControl('below');
 
-  constructor(private tokenStorageService: TokenStorageService) { }
+  constructor(private readonly tokenStorageService: TokenStorageService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly userService: UserService) { }
 
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
@@ -22,12 +28,25 @@ export class HeaderComponent implements OnInit {
     if (this.isLoggedIn) {
       const user = this.tokenStorageService.getUser();
       this.username = user.username;
+      this.userService.getProfilePicture().subscribe(data => {
+        this.avatar = data;
+      },
+        error => {
+          console.log(error);
+        });
     }
+  }
+
+  open_home_page(): void {
+    this.router.navigate(['/profile/' + this.username]).then(_page => {window.location.reload()});
+  }
+
+  open_favorites_page():void {
+    this.router.navigate([{ outlets: { favorites: ['profile', this.username, 'favorites'] } }]);
   }
 
   logout() {
     this.tokenStorageService.signOut();
-    window.location.reload();
   }
 
 }
