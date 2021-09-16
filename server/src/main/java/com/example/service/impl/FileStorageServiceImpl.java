5d1292dba2798b638b9fb579871ea1261d7c0640
bakedpotato191 <vladislavp@pm.me>
2021-09-17 01:03:42 +0200

    make it synchronous for the time being

--- server/src/main/java/com/example/service/impl/FileStorageServiceImpl.java
+++ server/src/main/java/com/example/service/impl/FileStorageServiceImpl.java
@@ -57,7 +57,6 @@ public class FileStorageServiceImpl implements FileStorageService {
 	}
 
 	@Override
-	@Async
 	public Publication save(MultipartFile file, String description) throws IncorrectFileExtensionException, IOException {
 
 		String fileContentType = file.getContentType();
